8f2b60ed18287a1e517b4edad06f435eb00e0a85
Animesh Sinha <animeshsinha1309@gmail.com>
2018-12-21 15:19:09 +0530

    Modules needed
    Added in the dependencies for the project and generated required modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IllustrationsComponent } from './illustrations/illustrations.component';
+import { FriendsComponent } from './friends/friends.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { StuffComponent } from './stuff/stuff.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IllustrationsComponent,
+    FriendsComponent,
+    HomeComponent,
+    ProjectsComponent,
+    StuffComponent
   ],
   imports: [
     BrowserModule,
